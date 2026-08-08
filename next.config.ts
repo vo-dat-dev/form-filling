17696d81fcaee79a22309bb9a4fa2ca7b3f48768
Võ Tiến Đạt <votiendat.dev@gmail.com>
2026-08-09 00:48:14 +0700

    fix(runtime): set ignoreBuildErrors from true to false

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: { ignoreBuildErrors: true },
+  typescript: { ignoreBuildErrors: false },
   serverExternalPackages: ["@copilotkit/runtime"],
   experimental: {},
   env: {
